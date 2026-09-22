1483c542be73e2e0725c1f1bdab54fffa5cad31f
Rideema14 <rideemasinghjbp@gmail.com>
2026-09-22 19:08:32 +0530

    deleted

--- backend/src/middleware/notFound.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-import { NotFoundError } from "../common/errors";
-
-export function notFoundHandler(req: Request, _res: Response, next: NextFunction) {
-  next(new NotFoundError(`No route matches ${req.method} ${req.originalUrl}`));
-}
